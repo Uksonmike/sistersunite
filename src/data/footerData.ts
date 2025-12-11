dfcbbc92ef6fd14e7f2d547d6e31c372d1cc81e8
Uksonmike <michaelukpongson91@gmail.com>
2025-12-11 19:29:50 +0100

    fixed build bug

--- src/data/footerData.ts
+++ src/data/footerData.ts
@@ -1,48 +1,76 @@
 interface DataType {
-   id:number;
-   page:string;
-   widget_title: string;
-   footer_link: {
-      link: string;
-      link_title: string;
-   }[];
-
+  id: number;
+  page: string;
+  widget_title: string;
+  footer_link: {
+    link: string;
+    link_title: string;
+  }[];
 }
 
 const footer_data: DataType[] = [
-   {
-      id:1,
-      page:"home_1",
-      widget_title: "About",
-      footer_link: [{ link: "/", link_title: "Home" }, { link: "/about", link_title: "About Us" }, { link: "/causes", link_title: "Our Programs" }, { link: "/contact", link_title: "Contact Us" }, { link: "https://flutterwave.com/donate/plyjpesfl4wx", link_title: "Donate" },{ link: "/sisters-unite-brochure.pdf", link_title: "Our Brochure" },,]
-   },
-   {
-      id:2,
-      page:"home_1",
-      widget_title: "Our Programs",
-      footer_link: [{ link: "#", link_title: "Back-to-School Drive" }, { link: "#", link_title: "Mothers of Influence" }, { link: "#", link_title: " Becoming a Better You (BABY)" }, { link: "#", link_title: "Community Outreach Projects" },]
-   },
-   {
-      id:3,
-      page:"home_1",
-      widget_title: "Explore",
-      footer_link: [{ link: "#", link_title: "E-Brochure" }, { link: "#", link_title: "Contact Us" }, { link: "#", link_title: "Gallery" },]
-   },
+  {
+    id: 1,
+    page: "home_1",
+    widget_title: "About",
+    footer_link: [
+      { link: "/", link_title: "Home" },
+      { link: "/about", link_title: "About Us" },
+      { link: "/causes", link_title: "Our Programs" },
+      { link: "/contact", link_title: "Contact Us" },
+      {
+        link: "https://flutterwave.com/donate/plyjpesfl4wx",
+        link_title: "Donate",
+      },
+      { link: "/sisters-unite-brochure.pdf", link_title: "Our Brochure" },
+    ],
+  },
+  {
+    id: 2,
+    page: "home_1",
+    widget_title: "Our Programs",
+    footer_link: [
+      { link: "#", link_title: "Back-to-School Drive" },
+      { link: "#", link_title: "Mothers of Influence" },
+      { link: "#", link_title: " Becoming a Better You (BABY)" },
+      { link: "#", link_title: "Community Outreach Projects" },
+    ],
+  },
+  {
+    id: 3,
+    page: "home_1",
+    widget_title: "Explore",
+    footer_link: [
+      { link: "#", link_title: "E-Brochure" },
+      { link: "#", link_title: "Contact Us" },
+      { link: "#", link_title: "Gallery" },
+    ],
+  },
 
-   // home two
+  // home two
 
-   {
-      id:1,
-      page:"home_2",
-      widget_title: "About",
-      footer_link: [{ link: "#", link_title: "Home" }, { link: "#", link_title: "Donation" }, { link: "#", link_title: "About us" }, { link: "#", link_title: "Event" },]
-   },
-   {
-      id:2,
-      page:"home_2",
-      widget_title: "Quick links",
-      footer_link: [{ link: "#", link_title: "Causes" }, { link: "#", link_title: "About" }, { link: "#", link_title: "New campaign" }, { link: "#", link_title: "Site map" },]
-   },
-]
+  {
+    id: 1,
+    page: "home_2",
+    widget_title: "About",
+    footer_link: [
+      { link: "#", link_title: "Home" },
+      { link: "#", link_title: "Donation" },
+      { link: "#", link_title: "About us" },
+      { link: "#", link_title: "Event" },
+    ],
+  },
+  {
+    id: 2,
+    page: "home_2",
+    widget_title: "Quick links",
+    footer_link: [
+      { link: "#", link_title: "Causes" },
+      { link: "#", link_title: "About" },
+      { link: "#", link_title: "New campaign" },
+      { link: "#", link_title: "Site map" },
+    ],
+  },
+];
 
 export default footer_data;
--- src/layout/footers/FooterOne.tsx
+++ src/layout/footers/FooterOne.tsx
@@ -1,4 +1,4 @@
-import Image, { StaticImageData } from "next/image"
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import SocialIcon from "@/components/common/SocialIcon";
 import footer_data from "@/data/footerData";
@@ -14,76 +14,97 @@ import footerGallery_5 from "@/assets/img/footer/gallery5.jpg";
 import footerGallery_6 from "@/assets/img/footer/gallery6.jpg";
 
 interface ContentData {
-   footer_about_text: JSX.Element;
-   gallery: StaticImageData[];
+  footer_about_text: JSX.Element;
+  gallery: StaticImageData[];
 }
 
 const footer_content: ContentData = {
-   footer_about_text: (<>Sisters United for Children –  www.sistersunite.org.ng – is a group of friends and professional Christian women who have distinguished themselves in various fields of endeavor cutting across Banking/Finance, Pharmacy, Education, Diplomatic Corps, Public Service etc. </>),
-   gallery: [footerGallery_1, footerGallery_2, footerGallery_3, footerGallery_4, footerGallery_5, footerGallery_6],
-}
+  footer_about_text: (
+    <>
+      Sisters United for Children – www.sistersunite.org.ng – is a group of
+      friends and professional Christian women who have distinguished themselves
+      in various fields of endeavor cutting across Banking/Finance, Pharmacy,
+      Education, Diplomatic Corps, Public Service etc.{" "}
+    </>
+  ),
+  gallery: [
+    footerGallery_1,
+    footerGallery_2,
+    footerGallery_3,
+    footerGallery_4,
+    footerGallery_5,
+    footerGallery_6,
+  ],
+};
 
 const { footer_about_text, gallery } = footer_content;
 
 const FooterOne = () => {
-   return (
-      <footer className="footer-area overlay text-white pt-120 bgs-cover" style={{ backgroundImage: `url(/assets/img/footer/footer-bg.jpg)` }}>
-         <div className="container">
-            <div className="row justify-content-between">
-               <div className="col-lg-3 col-md-6 col-sm-8">
-                  <div className="widget widget_about">
-                     <div className="logo_footer mb-25">
-                        <Link href="/">
-                           <Image src={logo_1} alt="Logo" />
-                        </Link>
-                     </div>
-                     <p>{footer_about_text}</p>
-                     <div className="social-style-one pt-20">
-                        <SocialIcon />
-                     </div>
-                  </div>
-               </div>
-
-               {footer_data.filter((item) => item.page === "home_1").map((item) => (
-                  <div key={item.id} className="col-lg-2 col-md-3 col-sm-4 col-6">
-                     <div className="widget widget_nav_menu">
-                        <h5 className="widget-title">{item.widget_title}</h5>
-                        <ul>
-                           {item.footer_link.map((li, i) => (
-                              <li key={i}><Link href={li.link}>{li.link_title}</Link></li>
-                           ))}
-                        </ul>
-                     </div>
-                  </div>
-               ))}
-
-               <div className="col-lg-3 col-md-6 col-sm-8">
-                  <div className="widget widget_gallery">
-                     <h4 className="widget-title">Photo Gallery</h4>
-                     <div className="gallery-photos">
-                        {gallery.map((gallery, index) => (
-                           <a style={{cursor:"pointer"}} key={index} ><Image src={gallery} alt="Gallery" /></a>
-                        ))}
-                     </div>
-                  </div>
-               </div>
+  return (
+    <footer
+      className="footer-area overlay text-white pt-120 bgs-cover"
+      style={{ backgroundImage: `url(/assets/img/footer/footer-bg.jpg)` }}
+    >
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-lg-3 col-md-6 col-sm-8">
+            <div className="widget widget_about">
+              <div className="logo_footer mb-25">
+                <Link href="/">
+                  <Image src={logo_1} alt="Logo" />
+                </Link>
+              </div>
+              <p>{footer_about_text}</p>
+              <div className="social-style-one pt-20">
+                <SocialIcon />
+              </div>
             </div>
-         </div>
-         
-         <div className="footer-bottom mt-50">
-            <div className="container">
-               <div className="footer-bottom__inner">
-                  <div className="donate-by">
-                    
-                  </div>
-                  <div className="copyright">
-                     <p>Copyright 2025 All Right Reserved</p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
-   )
-}
+          </div>
 
-export default FooterOne
+          {footer_data
+            .filter((item) => item.page === "home_1")
+            .map((item) => (
+              <div key={item.id} className="col-lg-2 col-md-3 col-sm-4 col-6">
+                <div className="widget widget_nav_menu">
+                  <h5 className="widget-title">{item.widget_title}</h5>
+                  <ul>
+                    {item.footer_link.map((li, i) => (
+                      <li key={i}>
+                        <Link href={li.link}>{li.link_title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+
+          <div className="col-lg-3 col-md-6 col-sm-8">
+            <div className="widget widget_gallery">
+              <h4 className="widget-title">Photo Gallery</h4>
+              <div className="gallery-photos">
+                {gallery.map((gallery, index) => (
+                  <a style={{ cursor: "pointer" }} key={index}>
+                    <Image src={gallery} alt="Gallery" />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom mt-50">
+        <div className="container">
+          <div className="footer-bottom__inner">
+            <div className="donate-by"></div>
+            <div className="copyright">
+              <p>Copyright 2025 All Right Reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterOne;
