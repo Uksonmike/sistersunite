3533f405c04470d946c5e558bc5d16443f14c1b3
Uksonmike <michaelukpongson91@gmail.com>
2025-12-14 18:12:14 +0100

    fixed type error

--- src/data/volunteerData.ts
+++ src/data/volunteerData.ts
@@ -26,163 +26,164 @@ import volunteer3Thumb_7 from "@/assets/img/valunteer/volunteer-three7.jpg";
 import volunteer3Thumb_8 from "@/assets/img/valunteer/volunteer-three8.jpg";
 
 interface DataType {
-   id: number;
-   page: string;
-   thumb: StaticImageData;
-   name: string;
-   item_bg?: string;
+  id: number;
+  page: string;
+  thumb: StaticImageData;
+  name: string;
+  item_bg?: string;
+  designation?: string;
 }
 
 const volunteer_data: DataType[] = [
-   {
-      id: 1,
-      page: "home_1",
-      thumb: volunteerThumb_1,
-      name: "Olufemi Bola-Sadipe",
-      designation: "Board of Advisor",
-      item_bg: "valunteer-item--green",
-   },
-   {
-      id: 2,
-      page: "home_1",
-      thumb: volunteerThumb_2,
-      designation: "Program Coordinator",
-      name: "Chidera Obi"
-   },
-   {
-      id: 3,
-      page: "home_1",
-      thumb: volunteerThumb_3,
-      name: "Yetunde Onanuga",
-      designation: "Member",
-      item_bg: "valunteer-item--yellow",
-   },
-   {
-      id: 4,
-      page: "home_1",
-      thumb: volunteerThumb_4,
-      name: "Guy Hawkins",
-      item_bg: "valunteer-item--green",
-   },
-   {
-      id: 5,
-      page: "home_1",
-      thumb: volunteerThumb_5,
-      name: "Theresa Webb"
-   },
-   {
-      id: 6,
-      page: "home_1",
-      thumb: volunteerThumb_6,
-      name: "Brooklyn Simmons",
-      item_bg: "valunteer-item--yellow",
-   },
+  {
+    id: 1,
+    page: "home_1",
+    thumb: volunteerThumb_1,
+    name: "Olufemi Bola-Sadipe",
+    designation: "Board of Advisor",
+    item_bg: "valunteer-item--green",
+  },
+  {
+    id: 2,
+    page: "home_1",
+    thumb: volunteerThumb_2,
+    designation: "Program Coordinator",
+    name: "Chidera Obi",
+  },
+  {
+    id: 3,
+    page: "home_1",
+    thumb: volunteerThumb_3,
+    name: "Yetunde Onanuga",
+    designation: "Member",
+    item_bg: "valunteer-item--yellow",
+  },
+  {
+    id: 4,
+    page: "home_1",
+    thumb: volunteerThumb_4,
+    name: "Guy Hawkins",
+    item_bg: "valunteer-item--green",
+  },
+  {
+    id: 5,
+    page: "home_1",
+    thumb: volunteerThumb_5,
+    name: "Theresa Webb",
+  },
+  {
+    id: 6,
+    page: "home_1",
+    thumb: volunteerThumb_6,
+    name: "Brooklyn Simmons",
+    item_bg: "valunteer-item--yellow",
+  },
 
-   // home two
+  // home two
 
-   {
-      id: 1,
-      page: "home_2",
-      thumb: volunteer2Thumb_1,
-      name: "Brooklyn Simmons"
-   },
-   {
-      id: 2,
-      page: "home_2",
-      thumb: volunteer2Thumb_2,
-      name: "Savannah Nguyen",
-      item_bg: "valunteer-two-item__des--yellow",
-   },
-   {
-      id: 3,
-      page: "home_2",
-      thumb: volunteer2Thumb_3,
-      name: "Darrell Steward",
-      item_bg: "valunteer-two-item__des--green",
-   },
-   {
-      id: 4,
-      page: "home_2",
-      thumb: volunteer2Thumb_4,
-      name: "Leslie Alexander"
-   },
-   {
-      id: 5,
-      page: "home_2",
-      thumb: volunteer2Thumb_5,
-      name: "Brooklyn Simmons"
-   },
-   {
-      id: 6,
-      page: "home_2",
-      thumb: volunteer2Thumb_6,
-      name: "Savannah Nguyen",
-      item_bg: "valunteer-two-item__des--yellow",
-   },
-   {
-      id: 7,
-      page: "home_2",
-      thumb: volunteer2Thumb_7,
-      name: "Darrell Steward",
-      item_bg: "valunteer-two-item__des--green",
-   },
-   {
-      id: 8,
-      page: "home_2",
-      thumb: volunteer2Thumb_8,
-      name: "Leslie Alexander"
-   },
+  {
+    id: 1,
+    page: "home_2",
+    thumb: volunteer2Thumb_1,
+    name: "Brooklyn Simmons",
+  },
+  {
+    id: 2,
+    page: "home_2",
+    thumb: volunteer2Thumb_2,
+    name: "Savannah Nguyen",
+    item_bg: "valunteer-two-item__des--yellow",
+  },
+  {
+    id: 3,
+    page: "home_2",
+    thumb: volunteer2Thumb_3,
+    name: "Darrell Steward",
+    item_bg: "valunteer-two-item__des--green",
+  },
+  {
+    id: 4,
+    page: "home_2",
+    thumb: volunteer2Thumb_4,
+    name: "Leslie Alexander",
+  },
+  {
+    id: 5,
+    page: "home_2",
+    thumb: volunteer2Thumb_5,
+    name: "Brooklyn Simmons",
+  },
+  {
+    id: 6,
+    page: "home_2",
+    thumb: volunteer2Thumb_6,
+    name: "Savannah Nguyen",
+    item_bg: "valunteer-two-item__des--yellow",
+  },
+  {
+    id: 7,
+    page: "home_2",
+    thumb: volunteer2Thumb_7,
+    name: "Darrell Steward",
+    item_bg: "valunteer-two-item__des--green",
+  },
+  {
+    id: 8,
+    page: "home_2",
+    thumb: volunteer2Thumb_8,
+    name: "Leslie Alexander",
+  },
 
-   // home three
+  // home three
 
-   {
-      id: 1,
-      page: "home_3",
-      thumb: volunteer3Thumb_1,
-      name: "Brooklyn Simmons"
-   },
-   {
-      id: 2,
-      page: "home_3",
-      thumb: volunteer3Thumb_2,
-      name: "Savannah Nguyen"
-   },
-   {
-      id: 3,
-      page: "home_3",
-      thumb: volunteer3Thumb_3,
-      name: "Darrell Steward"
-   },
-   {
-      id: 4,
-      page: "home_3",
-      thumb: volunteer3Thumb_4,
-      name: "Leslie Alexander"
-   },
-   {
-      id: 5,
-      page: "home_3",
-      thumb: volunteer3Thumb_5,
-      name: "Brooklyn Simmons"
-   },
-   {
-      id: 6,
-      page: "home_3",
-      thumb: volunteer3Thumb_6,
-      name: "Savannah Nguyen"
-   },
-   {
-      id: 7,
-      page: "home_3",
-      thumb: volunteer3Thumb_7,
-      name: "Darrell Steward"
-   },
-   {
-      id: 8,
-      page: "home_3",
-      thumb: volunteer3Thumb_8,
-      name: "Leslie Alexander"
-   },
-]
+  {
+    id: 1,
+    page: "home_3",
+    thumb: volunteer3Thumb_1,
+    name: "Brooklyn Simmons",
+  },
+  {
+    id: 2,
+    page: "home_3",
+    thumb: volunteer3Thumb_2,
+    name: "Savannah Nguyen",
+  },
+  {
+    id: 3,
+    page: "home_3",
+    thumb: volunteer3Thumb_3,
+    name: "Darrell Steward",
+  },
+  {
+    id: 4,
+    page: "home_3",
+    thumb: volunteer3Thumb_4,
+    name: "Leslie Alexander",
+  },
+  {
+    id: 5,
+    page: "home_3",
+    thumb: volunteer3Thumb_5,
+    name: "Brooklyn Simmons",
+  },
+  {
+    id: 6,
+    page: "home_3",
+    thumb: volunteer3Thumb_6,
+    name: "Savannah Nguyen",
+  },
+  {
+    id: 7,
+    page: "home_3",
+    thumb: volunteer3Thumb_7,
+    name: "Darrell Steward",
+  },
+  {
+    id: 8,
+    page: "home_3",
+    thumb: volunteer3Thumb_8,
+    name: "Leslie Alexander",
+  },
+];
 
 export default volunteer_data;
